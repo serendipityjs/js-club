8d87a3c946b2a00440262441e9bdcc7ef133ab9f
js <1334003601@qq.com>
2024-06-26 12:12:01 +0800

    题目分类开发!

---
+++ js-club-subject/js-club-subject-domain/target/generated-sources/annotations/com/js/subject/domain/convert/SubjectCategoryConvertImpl.java
@@ -0,0 +1,62 @@
+package com.js.subject.domain.convert;
+
+import com.js.subject.domain.entity.SubjectCategoryBo;
+import com.js.subject.infrastructure.basic.entity.SubjectCategory;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-06-26T11:53:59+0800",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 18 (Oracle Corporation)"
+)
+public class SubjectCategoryConvertImpl implements SubjectCategoryConvert {
+
+    @Override
+    public SubjectCategory convertToCategory(SubjectCategoryBo subjectCategoryBo) {
+        if ( subjectCategoryBo == null ) {
+            return null;
+        }
+
+        SubjectCategory subjectCategory = new SubjectCategory();
+
+        subjectCategory.setId( subjectCategoryBo.getId() );
+        subjectCategory.setCategoryName( subjectCategoryBo.getCategoryName() );
+        subjectCategory.setCategoryType( subjectCategoryBo.getCategoryType() );
+        subjectCategory.setImageUrl( subjectCategoryBo.getImageUrl() );
+        subjectCategory.setParentId( subjectCategoryBo.getParentId() );
+
+        return subjectCategory;
+    }
+
+    @Override
+    public List<SubjectCategoryBo> convertToBoList(List<SubjectCategory> subjectCategories) {
+        if ( subjectCategories == null ) {
+            return null;
+        }
+
+        List<SubjectCategoryBo> list = new ArrayList<SubjectCategoryBo>( subjectCategories.size() );
+        for ( SubjectCategory subjectCategory : subjectCategories ) {
+            list.add( subjectCategoryToSubjectCategoryBo( subjectCategory ) );
+        }
+
+        return list;
+    }
+
+    protected SubjectCategoryBo subjectCategoryToSubjectCategoryBo(SubjectCategory subjectCategory) {
+        if ( subjectCategory == null ) {
+            return null;
+        }
+
+        SubjectCategoryBo subjectCategoryBo = new SubjectCategoryBo();
+
+        subjectCategoryBo.setId( subjectCategory.getId() );
+        subjectCategoryBo.setCategoryName( subjectCategory.getCategoryName() );
+        subjectCategoryBo.setCategoryType( subjectCategory.getCategoryType() );
+        subjectCategoryBo.setImageUrl( subjectCategory.getImageUrl() );
+        subjectCategoryBo.setParentId( subjectCategory.getParentId() );
+
+        return subjectCategoryBo;
+    }
+}
